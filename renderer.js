a6433a5219af8a15d43122298e4ccf41e7f1bdb9
Alexander Mancevice <amancevice@cargometrics.com>
2018-03-18 09:13:51 -0400

    Dark Theme

--- renderer.js
+++ renderer.js
@@ -6,10 +6,11 @@ const { remote, ipcRenderer } = require('electron')
 
 const shell = remote.shell
 
-// webview.addEventListener('dom-ready', () => {
+webview.addEventListener('dom-ready', () => {
   // add modified twitter darkmode styles here!
-  // webview.insertCSS('html{overflow-x:hidden;}')
-// })
+  // https://userstyles.org/styles/152421/dark-mobile-twitter
+  webview.insertCSS('[role="main"] span,[role="banner"] span,textarea,[dir="auto"]{color:#eaeaea !important}[role="button"][type="button"] div{color:black !important}[role="main"],[role="main"] div{background-color:#213548 !important}[role="banner"] div,[role="banner"] a{background-color:#21303f !important}[role="article"] a > div,[aria-label^="Timeline"] > div > div > div{border-color:#324e62 !important}')
+})
 
 webview.addEventListener('new-window', e => {
   e.preventDefault()
